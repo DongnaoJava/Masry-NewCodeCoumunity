ad0ac2b949008fe3213f073b1d90d9ab69d5fe50
jb <masterooon@shu.edu.cn>
2022-02-21 10:55:02 +0800

    forth

--- community/src/main/java/com/bin/controller/LoginController.java
+++ community/src/main/java/com/bin/controller/LoginController.java
@@ -1,12 +1,62 @@
 package com.bin.controller;
 
+import com.bin.bean.ActivationConsequence;
+import com.bin.bean.User;
+import com.bin.service.impl.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Map;
 
 @Controller
 public class LoginController {
+    @Autowired
+    UserService userService;
+
     @GetMapping("/register")
-    public String Register() {
+    public String getRegisterHtml() {
         return "site/register";
+    }
+
+    @GetMapping("/login")
+    public String getLogin() {
+        return "site/login";
+    }
+
+    @PostMapping("/register")
+    public String register(User user, Model model) {
+        Map<String, String> mapInfo = userService.judgeUserRegisterInfo(user);
+        if (mapInfo == null | mapInfo.isEmpty()) {
+            model.addAttribute("successInfo", "已经向您邮箱发送了一封邮件，请您确认注册账号信息！");
+            model.addAttribute("target", "/index");
+            return "/site/operate-result";
+        } else {
+            model.addAttribute("accountInfo", mapInfo.get("accountInfo"));
+            model.addAttribute("passwordInfo", mapInfo.get("passwordInfo"));
+            model.addAttribute("emailInfo", mapInfo.get("emailInfo"));
+            model.addAttribute("user", user);
+            return "site/register";
+        }
+    }
+
+    @GetMapping("/activation/{id}/{activationCode}")
+    public String activateAccount(@PathVariable("id") Integer id, @PathVariable("activationCode") String activationCode, Model model) {
+        Enum<ActivationConsequence> activationConsequence = userService.activation(id, activationCode);
+        if (ActivationConsequence.ACTIVATION_SUCCESS.equals(activationConsequence)) {
+            model.addAttribute("successInfo", "您的账号已经激活成功,可以正常使用了！");
+            model.addAttribute("target", "/login");
+        } else if (ActivationConsequence.ACTIVATION_REPEAT.equals(activationConsequence)) {
+            model.addAttribute("successInfo", "您的账号已经激活过了,无需重复激活！");
+            model.addAttribute("target", "/login");
+        } else {
+            model.addAttribute("successInfo", "您的账号信息未激活成功，请您查询是否注册成功！");
+            model.addAttribute("target", "/register");
+        }
+        System.out.println(userService.selectUserById(id));
+        return "/site/operate-result";
     }
 }
--- community/src/main/java/com/bin/service/impl/UserService.java
+++ community/src/main/java/com/bin/service/impl/UserService.java
@@ -1,14 +1,17 @@
 package com.bin.service.impl;
 
+import com.bin.bean.ActivationConsequence;
 import com.bin.bean.User;
 import com.bin.dao.UserMapper;
+import com.bin.util.mailUtil.MailSendUtil;
 import com.bin.util.registerUtil.Md5Util;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +20,12 @@ import java.util.Map;
 public class UserService implements UserMapper {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    MailSendUtil mailSendUtil;
+    @Autowired
+    TemplateEngine templateEngine;
+    @Value("${community.path.domain}")
+    String domain;
 
     //根据id查询用户所有信息
     @Override
@@ -68,43 +77,77 @@ public class UserService implements UserMapper {
         else {
             //判断账号为空
             if (StringUtils.isBlank(user.getUsername())) {
-                mapInfo.put("Info", "账号不能为空！");
+                mapInfo.put("accountInfo", "账号不能为空！");
                 return mapInfo;
             }
             //判断密码为空
             if (StringUtils.isBlank(user.getPassword())) {
-                mapInfo.put("Info", "密码不能为空！");
+                mapInfo.put("passwordInfo", "密码不能为空！");
                 return mapInfo;
             }
             //判断邮箱为空
             if (StringUtils.isBlank(user.getEmail())) {
-                mapInfo.put("Info", "邮箱不能为空！");
+                mapInfo.put("emailInfo", "邮箱不能为空！");
                 return mapInfo;
             }
             //判断账号已存在
             String username = user.getUsername();
             User userExistedForUsername = userMapper.selectUserByName(username);
             if (userExistedForUsername != null) {
-                mapInfo.put("Info", "账号已存在！");
+                mapInfo.put("accountInfo", "账号已存在！");
                 return mapInfo;
             }
             //判断邮箱已注册
             String email = user.getEmail();
             User userExistedForMail = userMapper.selectUserByEmail(email);
             if (userExistedForMail != null) {
-                mapInfo.put("Info", "邮箱已经被注册！");
+                mapInfo.put("emailInfo", "邮箱已经被注册！");
                 return mapInfo;
             }
             //判断两次密码不相同
-
+            {
+                // 已经在前端页面中实现控制
+            }
             //注册用户
-            String password = user.getPassword();
-            String salt = Md5Util.generateUUID().substring(0, 5);
-            String secretPassword = Md5Util.Md5(password + salt);
-            user.setPassword(secretPassword);
+            user.setSalt(Md5Util.generateUUID().substring(0, 5));
+            user.setPassword(Md5Util.Md5(user.getPassword() + user.getSalt()));
+            user.setType(0);
+            user.setStatus(0);
+            user.setActivationCode(Md5Util.generateUUID());
+            user.setHeaderUrl("http://api.btstu.cn/sjtx/api.php?lx=c1&format=images");
             user.setCreateTime(new Date());
-            user.set
+            userMapper.insertUser(user);
+
+            //向这个html模板文件中传入参数，需要借助这个context，模板引擎会自动根据传入的参数整合到最终的html文件中
+            Context context = new Context();
+            context.setVariable("SendTo", user.getUsername());
+            String url = domain + "/activation/" + user.getId() + "/" + user.getActivationCode();
+            context.setVariable("url", url);
+            String content = templateEngine.process("/mail/activation.html", context);
+            mailSendUtil.Sendmail(user.getEmail(), "Activate your account!", content);
         }
         return mapInfo;
     }
+
+    public Enum<ActivationConsequence> activation(Integer id, String activationCode) {
+        User user = userMapper.selectUserById(id);
+        if (user != null && user.getActivationCode() != null && user.getActivationCode().equals(activationCode)) {
+            if (user.getStatus() == 0) {
+                userMapper.updateStatus(id,1);
+                return ActivationConsequence.ACTIVATION_SUCCESS;
+            } else
+                return ActivationConsequence.ACTIVATION_REPEAT;
+        } else {
+            return ActivationConsequence.ACTIVATION_FAILURE;
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
